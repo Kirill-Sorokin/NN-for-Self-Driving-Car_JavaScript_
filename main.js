c1e792bca7c82b3720f373ec6c9df58477c7be95
Kirill David Sorokin <96061027+Kirill-Sorokin@users.noreply.github.com>
2024-03-29 18:38:36 +0000

    moving the "canvas.height = window.innerHeight;" to refresh page.

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const canvas = document.getElementById("myCanvas");
-canvas.height = window.innerHeight;
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
@@ -9,11 +8,12 @@ car.draw(ctx);
 animate();
 
 /**
- * requestionAnimationFrame method recalls the animate method
+ * requestionAnimationFrame recalls the animate method
  * many times, giving the illusion of 'moving'.
  */
 function animate(){
     car.update();
+    canvas.height = window.innerHeight;
     car.draw(ctx);
     requestAnimationFrame(animate);
 }
