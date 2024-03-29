a2544eeb85873333a22ac23ca772476447fb52fb
Kirill David Sorokin <96061027+Kirill-Sorokin@users.noreply.github.com>
2024-03-29 19:05:29 +0000

    creating lanes.

--- self driving car/car.js
+++ self driving car/car.js
@@ -13,6 +13,10 @@ class Car{
     }
 
     update(){
+        this.#move();
+    }
+
+    #move(){
         if(this.controls.forward){
             this.speed += this.acceleration;
         }
@@ -34,13 +38,15 @@ class Car{
         if(Math.abs(this.speed) < this.friction){
             this.speed = 0;
         }
-        if(this.controls.right){
-            this.angle -= 0.03;
+        if(this.speed != 0){
+            const flip = this.speed > 0?1: -1;
+            if(this.controls.right){
+                this.angle -= 0.03 * flip;
+            }
+            if(this.controls.left){
+                this.angle += 0.03 * flip;
+            }
         }
-        if(this.controls.left){
-            this.angle += 0.03;
-        }
-
         this.x -= Math.sin(this.angle) * this.speed;
         this.y -= Math.cos(this.angle) * this.speed;
         // this.y -= this.speed;
--- self driving car/controls.js
+++ self driving car/controls.js
@@ -23,7 +23,7 @@ class Controls{
                     this.reverse = true;
                     break;
             }
-            console.table(this);   
+            // console.table(this); for debugging
         }
         document.onkeyup = (event)=>{
             switch(event.key){
@@ -40,7 +40,7 @@ class Controls{
                     this.reverse = false;
                     break;
             }
-            console.table(this);  
+            // console.table(this);  
         }
     }
 }
