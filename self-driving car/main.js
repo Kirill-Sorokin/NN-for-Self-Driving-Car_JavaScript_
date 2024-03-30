d0be2805741ef65716da45fe55d33bd3d87e317a
Kirill David Sorokin <96061027+Kirill-Sorokin@users.noreply.github.com>
2024-03-30 18:19:40 +0000

    implement a traffic feature (simulating the traffic).

--- self-driving car/main.js
+++ self-driving car/main.js
@@ -3,20 +3,29 @@ canvas.width=200;
 
 const ctx = canvas.getContext("2d");
 const road=new Road(canvas.width/2,canvas.width*0.9);
-const car=new Car(road.getLaneCenter(1),100,25,47);
+const car=new Car(road.getLaneCenter(1),100,25,47, "KEYS"); //our car
+const traffic = [
+    new Car(road.getLaneCenter(1), -100,25,47, "DUMMY", 2.7) //simple behaviour
+];
 
 animate();
 
 function animate(){
-    car.update(road.borders);
+    for(let i = 0; i < traffic.length; i++){
+        traffic[i].update(road.borders, []);
+    }
+    car.update(road.borders, traffic);
 
     canvas.height=window.innerHeight;
 
     ctx.save();
     ctx.translate(0,-car.y+canvas.height*0.7);
 
+    for(let i = 0; i < traffic.length; i++){
+        traffic[i].draw(ctx, "purple");
+    }
     road.draw(ctx);
-    car.draw(ctx);
+    car.draw(ctx, "blue");
 
     ctx.restore();
     requestAnimationFrame(animate);
